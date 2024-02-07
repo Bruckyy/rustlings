0246a01f9f428004200add44d33ad28039c8e70e
LesTravaux <bouncebloc95@gmail.com>
2024-02-07 20:25:47 +0100

    66%

--- exercises/15_traits/traits5.rs
+++ exercises/15_traits/traits5.rs
@@ -7,7 +7,7 @@
 // Execute `rustlings hint traits5` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+/// to pass to the function an argument with the traits `SomeTrait` and `OtherTrait` implemented by all thestructs defined we juste have to indicate it with this syntax: impl <> + <>
 
 pub trait SomeTrait {
     fn some_function(&self) -> bool {
@@ -30,7 +30,7 @@ impl SomeTrait for OtherStruct {}
 impl OtherTrait for OtherStruct {}
 
 // YOU MAY ONLY CHANGE THE NEXT LINE
-fn some_func(item: ??) -> bool {
+fn some_func(item: impl SomeTrait + OtherTrait) -> bool {
     item.some_function() && item.other_function()
 }
 
